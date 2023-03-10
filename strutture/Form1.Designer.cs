956cfc5cd48f776426c7d0a40fc323ac11ecb553
Sciusco.20197 <Sciusco.20197@LABI2-13PC23>
2023-03-10 13:36:12 +0100

    Aggiungere i file di progetto.

---
+++ strutture/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace form3_guida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nome = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.prezzo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.nuovoprezzo = new System.Windows.Forms.TextBox();
+            this.nuovonome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "prezzo";
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(31, 79);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(100, 20);
+            this.nome.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(396, 32);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(278, 364);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // prezzo
+            // 
+            this.prezzo.Location = new System.Drawing.Point(183, 78);
+            this.prezzo.Name = "prezzo";
+            this.prezzo.Size = new System.Drawing.Size(100, 20);
+            this.prezzo.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "nome";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "salva";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "salva";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // nuovoprezzo
+            // 
+            this.nuovoprezzo.Location = new System.Drawing.Point(183, 334);
+            this.nuovoprezzo.Name = "nuovoprezzo";
+            this.nuovoprezzo.Size = new System.Drawing.Size(100, 20);
+            this.nuovoprezzo.TabIndex = 7;
+            // 
+            // nuovonome
+            // 
+            this.nuovonome.Location = new System.Drawing.Point(34, 334);
+            this.nuovonome.Name = "nuovonome";
+            this.nuovonome.Size = new System.Drawing.Size(100, 20);
+            this.nuovonome.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "nome aggiornato";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "prezzo aggiornato";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "elemento da cercare";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nuovonome);
+            this.Controls.Add(this.nuovoprezzo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.prezzo);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.nome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox prezzo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox nuovoprezzo;
+        private System.Windows.Forms.TextBox nuovonome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ strutture/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form3_guida
+{
+    public partial class Form1 : Form
+    {
+        public struct prodotto
+        {
+            public string nome;
+            public float prezzo;
+        }
+        public prodotto[] p;
+        public int dim;
+        public Form1()
+        {
+            InitializeComponent();
+            p = new prodotto[100];
+            dim = 0;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //funzione visualizza stampa nella lista il nome e prezzo del prodotto
+        public void visualizza(prodotto[] pp)
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < dim; i++)
+            {
+                listView1.Items.Add(prodString(p[i]));
+            }
+
+        }
+        public string prodString(prodotto p)
+        {
+            return "Nome: " + p.nome + " prezzo: " + p.prezzo.ToString();
+        }
+        //bottone per far inserire i prodotti e prezzi
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p[dim].nome = nome.Text;
+            p[dim].prezzo = float.Parse(prezzo.Text);
+            dim++;
+            visualizza(p);
+
+        }
+        //lista visualizza i prodotti e il prezzo
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //funzione ricerca elemento
+
+        public void Modifica(prodotto[] pp)
+        {
+            for (int i = 0; i < dim; i++)
+            {
+                if (p[i].nome == textBox1.Text)
+                {
+                    p[i].nome = nuovonome.Text;
+                    p[i].prezzo = float.Parse(nuovoprezzo.Text);
+                }
+            }
+        }
+        //casella inserimento nome da cercare
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //casella inserimento nome prodotto da sostituire
+        private void nuovonome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //casella inserimento prezzo prodotto da sostituire
+        private void nuovoprezzo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //bottone per far aggiornare il nome e il prezzo
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Modifica(p);
+            visualizza(p);
+        }
+
+
+    }
+}
